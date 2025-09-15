f137de0e60a13e0b38102002b8e6e469f9318e8a
Paul Dederer <paul.dederer@jpcodelabs.de>
2025-09-15 14:41:36 +0200

    correct vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,3 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -9,9 +7,4 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   plugins: [vue(), vueJsx(), vueDevTools()],
   base: '/TaskTiger-Public/',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
 })
